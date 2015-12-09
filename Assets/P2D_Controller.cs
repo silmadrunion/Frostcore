43f4d0bf6b9bf9e60fe8b19ff06f14918fe1d997
Kimerth <motfoleas@yahoo.com>
2015-12-09 19:13:50 +0200

    Character Controller

---
+++ Assets/P2D_Controller.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof (P2D_Motor))]
+public class P2D_Controller : MonoBehaviour
+{
+
+    private P2D_Motor _Motor;
+    private bool isJumping;
+
+	void Awake() 
+    {
+        _Motor = GetComponent<P2D_Motor>();
+	}
+	
+	void Update() 
+    {
+	    if(!isJumping)
+        {
+            isJumping = Input.GetButtonDown("Jump");
+        }
+	}
+
+    void FixedUpdate()
+    {
+        var deadZone = 0.1f;
+        bool crouch = Input.GetKey(KeyCode.LeftControl);
+
+        float moveHorizontal = 0f;
+
+        if(Input.GetAxis("Horizontal") > deadZone || Input.GetAxis("Horizontal") < -deadZone)
+            moveHorizontal = Input.GetAxis("Horizontal");
+
+        _Motor.Move(moveHorizontal, isJumping, crouch);
+
+        isJumping = false;
+    }
+}
